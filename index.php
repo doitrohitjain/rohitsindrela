9728455ebb1621c5900b8803250352c820805582
DOITROHITJAIN <doitrohitjain@gmail.com>
2021-07-01 21:25:40 +0530

    First Sample

--- index.php
+++ index.php
@@ -1,3 +1,125 @@
-<?php 
-phpinfo();
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>रोहित संग सिंड्रेला का शुभ विवाह " पिंक सिटी(जयपुर) " मे दिनांक (16𝙩𝙝 फरवरी , 2021 ) को सम्पन हुआ</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <style>
+    /* Remove the navbar's default margin-bottom and rounded borders */ 
+    .navbar {
+      margin-bottom: 0;
+      border-radius: 0;
+    }
+    
+    /* Add a gray background color and some padding to the footer */
+    footer {
+      background-color: #f2f2f2;
+      padding: 25px;
+    }
+	
+	.custom {
+	  font-family: "Comic Sans MS", cursive, sans-serif;
+	  font-weight: 550;
+	}
+  </style>
+</head>
+<body>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#">RohitSindrela</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">रोहित संग सिंड्रेला</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right hide">
+        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div class="jumbotron">
+  <div class="container text-center">
+    <h3>रोहित संग सिंड्रेला का शुभ विवाह</h3>      
+    <p>रोहित संग सिंड्रेला का शुभ विवाह " पिंक सिटी(जयपुर) " मे दिनांक (16𝙩𝙝 फरवरी , 2021 ) को सम्पन हुआ। <br>
+
+इस शुभ विवाह की वीडियोग्राफी <br>
+
+𝘼 𝙧𝙤𝙢𝙖𝙣𝙩𝙞𝙘, 𝙡𝙤𝙫𝙚𝙡𝙮 𝙬𝙚𝙙𝙙𝙞𝙣𝙜 𝙚𝙫𝙚𝙣𝙩 𝙞𝙣 𝙩𝙝𝙚 𝙧𝙚𝙨𝙤𝙧𝙩 𝙤𝙛 "𝙏𝙝𝙚 𝙋𝙞𝙣𝙠 𝘾𝙞𝙩𝙮".  ❤️ ❤️</p>
+  </div>
+</div>
+  
+<div class="container-fluid bg-3 text-center">    
+  <h3 class="hide">Some of my Work</h3><br>
+  <div class="row">
+    <div class="col-sm-4">
+		<p class="custom">Rohit & Sinderla's 1st month engagement anniversary</p> 
+		<iframe  height="315" src="https://www.youtube.com/embed/LFFDlzkYCjk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen> 
+		</iframe>
+    </div>
+	<div class="col-sm-4">
+		<p class="custom">Rohit & Sinderla's 2nd month engagement anniversary for now, and many more to come.</p> 
+		<iframe  height="315" src="https://www.youtube.com/embed/Ml17aSWL0A4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+	<div class="col-sm-4">
+		<p class="custom">Rohit & Sinderla's 2nd month engagement anniversary. I spend with a loved one is a blessing of God.</p> 
+		<iframe  height="315" src="https://www.youtube.com/embed/VUozvl0frNc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+	<div class="col-sm-4">
+		<p class="custom">Rohit & Sinderla celebrating happy 3rd month of anniversary .. #Love #Jaipur #Enjoy #Fun</p> 
+		<iframe  height="315" src="https://www.youtube.com/embed/zEFTTOnG0xU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+	<div class="col-sm-4">
+		<p class="custom">#Rohsin Rohit & Sinderla 4th month engagement anniversary</p> 
+		<iframe  height="315" src="https://www.youtube.com/embed/QU3PrdobCms" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+	
+	<div class="col-sm-4">
+		<p class="custom">रोहित weds सिंड्रेला छाबड़ा,जयपुर(किशगनढ़ वाले) बोले तो -इस शुभ घड़ी में, एक मीठे रिश्ते की है शुरुआत</p> 
+		<iframe  height="315" src="https://www.youtube.com/embed/KNPuOZ5S8sU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+	</div>
+	<div class="col-sm-4">
+		<p class="custom">Arpit hourse riding #Arpit #Jain #RohitSindrela #16thFeb2021 #Marriage #ArpitJain</p> 
+		<iframe  height="315" src="https://www.youtube.com/embed/Y4TH6azeqkk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+	</div>
+  </div>
+</div><br>
+
+<div class="container-fluid bg-3 text-center hide">    
+  <div class="row">
+    <div class="col-sm-3">
+      <p>Some text..</p>
+      <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+    </div>
+    <div class="col-sm-3"> 
+      <p>Some text..</p>
+      <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+    </div>
+    <div class="col-sm-3"> 
+      <p>Some text..</p>
+      <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+    </div>
+    <div class="col-sm-3">
+      <p>Some text..</p>
+      <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+    </div>
+  </div>
+</div><br><br>
+
+<footer class="container-fluid text-center">
+  <p>Developed & Designed By : Mrs. Rohit Jain(Sr. Software Engineer)</p>
+</footer>
+
+</body>
+</html>
